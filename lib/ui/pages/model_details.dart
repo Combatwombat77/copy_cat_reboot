74dcaa81c0dead14ee7cd58c9ea19eb9752fe5c1
ishe19 <38261770+ishe19@users.noreply.github.com>
2020-02-06 19:30:26 +0700

    Update model_details.dart

--- lib/ui/pages/model_details.dart
+++ lib/ui/pages/model_details.dart
@@ -44,6 +44,14 @@ var items = [
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.modelTitle),
+        actions: <Widget>[
+          IconButton(
+            onPressed: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context) => Pages()));
+            },
+            icon: Icon(Icons.blur_on),
+          )
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Uidata.btnColor,
@@ -115,7 +123,128 @@ Widget cardView(String cardName, Icon icon){
 
 
 
+class Pages extends StatefulWidget {
+  @override
+  _PagesState createState() => _PagesState();
+}
 
+class _PagesState extends State<Pages> with SingleTickerProviderStateMixin {
+
+  TabController inAppTabController;
+
+
+   @override
+  void initState(){
+    super.initState();
+    inAppTabController = new TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose(){
+    inAppTabController.dispose();
+    super.dispose();
+  }
+
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(""),
+        centerTitle: true,
+        bottom: 
+        TabBar(
+          controller: inAppTabController,
+          tabs: <Widget>[
+            Tab(
+//              icon: Icon(Icons.pages),
+              child: Text("Challenge Mapping"),
+            ),
+            Tab(
+//              icon: Icon(Icons.business),
+              child: Text("Impact Gaps"),
+            ),
+            Tab(
+//              icon: Icon(Icons.business),
+              child: Text("Solutions Mapping"),
+            ),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: inAppTabController,
+        children: <Widget>[
+          Challenges(),
+          ImpactGap(),
+          Solutions()
+        ],
+      ),
+    );
+  }
+}
+
+
+
+class ImpactGap extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: MediaQuery.of(context).size.height,
+        child: Center(
+            child:Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+          Card(
+            // height: 200,
+            // width: 250.0,
+            color: Colors.blue,
+            child: Container(
+              padding: const EdgeInsets.all(10),
+              width: MediaQuery.of(context).size.width / 1.5,
+              child: Text("Q: What is missing that could close the gap between the challenge and the current solutions, where are opportunities for greater collective impact and what are the key lessons learned?"),),
+          ),
+          Divider(
+            color: Colors.black,
+          ),
+          Text("Guiding Questions", style: TextStyle(decoration: TextDecoration.underline, fontWeight: FontWeight.bold, fontSize: 23.0),),
+
+        ],),)
+      );
+  }
+}
+
+class Challenges extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: MediaQuery.of(context).size.height,
+        child: Center(
+            child:Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+          
+        ],),)
+      );
+  }
+}
+
+class Solutions extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: MediaQuery.of(context).size.height,
+        child: Center(
+            child:Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+          
+        ],),)
+      );
+  }
+}
 
 
 
